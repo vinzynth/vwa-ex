159c1fe46877f0d3761586e2ba1dbb9d8d1950a8
Christian Leopold <christian.leopold@student.tugraz.at>
2015-10-25 20:00:27 +0100

    Added initial Project configuration

---
+++ vwa-ex/graph/src/main/java/at/chrl/vwa/App.java
@@ -0,0 +1,13 @@
+package at.chrl.vwa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
